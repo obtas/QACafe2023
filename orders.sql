7943fd9a6270c6f5fc5001a190e7d6c51bc6e798
Satta Foday <samilafo@amazon.co.uk>
2023-01-24 10:32:32 +0000

    created orders sql file

---
+++ orders.sql
@@ -0,0 +1,16 @@
+-- order_id, customer_name, drink, size, extras, price,
+
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    customer_name VARCHAR(20) NOT NULL, 
+    drink VARCHAR(20) NOT NULL,
+    size VARCHAR(6) NOT NULL,
+    extras BOOLEAN NULL,
+    price INTEGER NOT NULL 
+);
+
+INSERT INTO order (customer_name, drink, size, extras, price) VALUES ('Abiodun', 'hot chocolate', 'medium', false, 2.95);
+INSERT INTO order (customer_name, drink, size, extras, price) VALUES ('Beth', 'mocha', 'medium', false, 3.95);
+INSERT INTO order (customer_name, drink, size, extras, price) VALUES ('Carl', 'latte', 'small', false, 3.95);
+INSERT INTO order (customer_name, drink, size, extras, price) VALUES ('Deyja', 'berrie smoothie', 'large', false, 4.95);
+INSERT INTO order (customer_name, drink, size, extras, price) VALUES ('Edie', 'americano', 'small', false, 2.95);
